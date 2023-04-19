336188d755dc12d3049ddee819091db68105a4b6
Hugo83-tecnico <hugo83_tecnico@hotmail.com>
2023-04-19 10:05:59 -0300

    Inserido URL

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData(){
-    const url = 'http://localhost:3000/profile';
+    const url = 'https://raw.githubusercontent.com/Hugo83-tecnico/Portfolio/master/data/profile.json';
     const fetching = await fetch(url);
     
     return await fetching.json();
